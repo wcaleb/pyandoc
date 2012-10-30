ffa5472730160f84b4eeb4ff537adb03e967206a
Yoav Ram <yoavram+github@gmail.com>
2012-10-30 22:51:05 +0200

    added to_pdf method that converts to pdf and returns the pdf file name

--- pandoc/core.py
+++ pandoc/core.py
@@ -1,11 +1,11 @@
 import subprocess
 from os.path import exists
 
-PANDOC_PATH = r'C:\Program Files (x86)\Pandoc\bin\pandoc.exe'
+PANDOC_PATH = r'pandoc.exe'
+
 
 class Document(object):
 	"""A formatted document."""
-	
 	INPUT_FORMATS = (
 		'native', 'markdown', 'markdown+lhs', 'rst', 
 		'rst+lhs', 'html', 'latex', 'latex+lhs'
@@ -16,7 +16,7 @@ class Document(object):
 		'docbook', 'opendocument', 'odt', 'epub', 
 		'latex', 'latex+lhs', 'context', 'texinfo', 
 		'man', 'markdown', 'markdown+lhs', 'plain', 
-		'rst', 'rst+lhs', 'mediawiki', 'rtf'
+		'rst', 'rst+lhs', 'mediawiki', 'rtf', 'pdf'
 	)
 
 	# TODO: Add odt, epub formats (requires file access, not stdout)
@@ -26,26 +26,30 @@ class Document(object):
 		self._format = None
 		self._register_formats()
 		self.arguments = []
+	
 
-        def bib(self, bibfile):
-                if not exists(bibfile):
-                        raise IOError("Bib file not found: " + bibfile)
+	def bib(self, bibfile):
+		if not exists(bibfile):
+			raise IOError("Bib file not found: %s" % bibfile)
 		self.add_argument("bibliography=%s" % bibfile)
 
-        def csl(self, cslfile):
-                if not exists(cslfile):
-                        raise IOError("CSL file not found: " + cslfile)
+
+	def csl(self, cslfile):
+		if not exists(cslfile):
+			raise IOError("CSL file not found: %s" % cslfile)
 		self.add_argument("csl=%s" % cslfile)
 
-        def abbr(self, abbrfile):
-                if not exists(abbrfile):
-                        raise IOError("Abbreviations file not found: " + abbrfile)
+	def abbr(self, abbrfile):
+		if not exists(abbrfile):
+			raise IOError("Abbreviations file not found: " + abbrfile)
 		self.add_argument("citation-abbreviations=%s" % abbrfile)
         
+
 	def add_argument(self, arg):
-                self.arguments.append("--%s" % arg)
-                return self.arguments
+		self.arguments.append("--%s" % arg)
+		return self.arguments
         
+
 	@classmethod
 	def _register_formats(cls):
 		"""Adds format properties."""
@@ -55,19 +59,36 @@ class Document(object):
 				(lambda x, fmt=fmt: cls._output(x, fmt)), # fget
 				(lambda x, y, fmt=fmt: cls._input(x, y, fmt)))) # fset
 	
+
 	def _input(self, value, format=None):
 		# format = format.replace('_', '+')
 		self._content = value
 		self._format = format
 	
+
 	def _output(self, format):
-                subprocess_arguments = [PANDOC_PATH, '--from=%s' % self._format, '--to=%s' % format]
-                subprocess_arguments.extend(self.arguments)
+		subprocess_arguments = [PANDOC_PATH, '--from=%s' % self._format, '--to=%s' % format]
+		subprocess_arguments.extend(self.arguments)
 
-                p = subprocess.Popen(
-                        subprocess_arguments,
-                        stdin=subprocess.PIPE, 
-                        stdout=subprocess.PIPE
-                )
-
+		p = subprocess.Popen(
+				subprocess_arguments,
+				stdin=subprocess.PIPE, 
+				stdout=subprocess.PIPE
+		)
 		return p.communicate(self._content)[0]
+
+
+	def to_pdf(self, output_filename):
+		subprocess_arguments = [PANDOC_PATH, '--from=%s' % self._format, '--output=%s' % output_filename]
+		subprocess_arguments.extend(self.arguments)
+
+		p = subprocess.Popen(
+				subprocess_arguments,
+				stdin=subprocess.PIPE, 
+				stdout=subprocess.PIPE
+		)	
+		p.communicate(self._content)
+		if exists(output_filename):
+			return output_filename
+		else:
+			raise IOError("Failed creating PDF file: %s" % output_filename)
